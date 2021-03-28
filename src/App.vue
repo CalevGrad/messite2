d39b09f6f70b866408bf34e2a7fc8445636d85e1
Egor <egoriky@gmail.com>
2021-03-28 22:30:33 +0300

    Add Mail, Login, css-styles, SiteHeader, auth.module.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
+  <div>
+    <SiteHeader/>
+    <router-view/>
+  </div>
 </template>
 
 <script>
 
 import router from "@/router/router";
+import SiteHeader from "@/components/SiteHeader";
 
 export default {
   name: 'App',
   router: router,
   components: {
+    SiteHeader,
   }
 }
 </script>
 
 <style>
+@import './style/main.css';
+@import './style/fontawesome/css/all.css';
+
+input[type="search"]::-webkit-search-decoration,
+input[type="search"]::-webkit-search-cancel-button,
+input[type="search"]::-webkit-search-results-button,
+input[type="search"]::-webkit-search-results-decoration {
+  display: none;
+}
+
+input[type="search"]::-ms-clear {
+  display: none;
+}
+
+#search-clean-button {
+  background: white;
+}
 </style>
---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="header">
+    <div class="name-text unselectable">GRADIENT MESSENGER</div>
+    <div class="description-and-exit-button">
+      <div class="description">
+<!--        <div id="descr-username">{{ user_descr.username }}</div>-->
+<!--        <div id="descr-id">id: {{ user_descr.id }}</div>-->
+        <div id="descr-username">username</div>
+        <div id="descr-id">id</div>
+      </div>
+      <button id="button-exit" class="exit-button"
+              v-on:click="$emit('click-exit-button')"
+      >
+        Выйти
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SiteHeader",
+  props: {
+    // user_descr: {
+    //   type: Object,
+    //   required: true
+    // }
+  }
+}
+</script>
